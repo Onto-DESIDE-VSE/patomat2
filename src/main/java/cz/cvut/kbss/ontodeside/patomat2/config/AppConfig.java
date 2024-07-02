c8c9b193b688950feba3a2cc16dece82edba1fa1
Martin Ledvinka <martin.ledvinka@fel.cvut.cz>
2024-07-02 09:21:11 +0200

    [GH-1] Cleanup user data on session destroy

---
+++ src/main/java/cz/cvut/kbss/ontodeside/patomat2/config/AppConfig.java
@@ -0,0 +1,16 @@
+package cz.cvut.kbss.ontodeside.patomat2.config;
+
+import cz.cvut.kbss.ontodeside.patomat2.service.FileStorageService;
+import jakarta.servlet.http.HttpSessionListener;
+import org.springframework.boot.web.servlet.ServletListenerRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public ServletListenerRegistrationBean<HttpSessionListener> sessionListener(FileStorageService fileStorageService) {
+        return new ServletListenerRegistrationBean<>(new SessionCleanupListener(fileStorageService));
+    }
+}
---
+++ src/main/java/cz/cvut/kbss/ontodeside/patomat2/config/SessionCleanupListener.java
@@ -0,0 +1,42 @@
+package cz.cvut.kbss.ontodeside.patomat2.config;
+
+
+import cz.cvut.kbss.ontodeside.patomat2.Constants;
+import cz.cvut.kbss.ontodeside.patomat2.service.FileStorageService;
+import jakarta.servlet.http.HttpSession;
+import jakarta.servlet.http.HttpSessionEvent;
+import jakarta.servlet.http.HttpSessionListener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+
+/**
+ * Cleans user data up after session ends.
+ */
+public class SessionCleanupListener implements HttpSessionListener {
+
+    private static final Logger LOG = LoggerFactory.getLogger(SessionCleanupListener.class);
+
+    private final FileStorageService fileStorageService;
+
+    public SessionCleanupListener(FileStorageService fileStorageService) {this.fileStorageService = fileStorageService;}
+
+
+    @Override
+    public void sessionCreated(HttpSessionEvent se) {
+        LOG.debug("Created new session {}.", se.getSession().getId());
+    }
+
+    @Override
+    public void sessionDestroyed(HttpSessionEvent se) {
+        final HttpSession session = se.getSession();
+        LOG.debug("Cleaning up destroyed session {}.", session.getId());
+        final String ontologyFile = (String) session.getAttribute(Constants.ONTOLOGY_FILE_ATTRIBUTE);
+        final List<String> patternFiles = (List<String>) session.getAttribute(Constants.PATTERN_FILES_ATTRIBUTE);
+        if (ontologyFile != null) {
+            fileStorageService.deleteFile(ontologyFile);
+        }
+        patternFiles.forEach(fileStorageService::deleteFile);
+    }
+}
