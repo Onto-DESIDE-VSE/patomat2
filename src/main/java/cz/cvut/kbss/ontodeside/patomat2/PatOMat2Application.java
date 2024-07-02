509e2b66f818bea10c65cdb73657228fb709164b
Martin Ledvinka <martin.ledvinka@fel.cvut.cz>
2024-07-02 09:05:15 +0200

    Implement error handling.

--- src/main/java/cz/cvut/kbss/ontodeside/patomat2/PatOMat2Application.java
+++ src/main/java/cz/cvut/kbss/ontodeside/patomat2/PatOMat2Application.java
@@ -2,8 +2,10 @@ package cz.cvut.kbss.ontodeside.patomat2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class PatOMat2Application {
 
 	public static void main(String[] args) {
---
+++ src/main/java/cz/cvut/kbss/ontodeside/patomat2/rest/handler/RestExceptionHandler.java
@@ -0,0 +1,37 @@
+package cz.cvut.kbss.ontodeside.patomat2.rest.handler;
+
+import cz.cvut.kbss.ontodeside.patomat2.exception.InvalidFileException;
+import cz.cvut.kbss.ontodeside.patomat2.exception.PatOMat2Exception;
+import jakarta.servlet.http.HttpServletRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class RestExceptionHandler {
+
+    private static final Logger LOG = LoggerFactory.getLogger(RestExceptionHandler.class);
+
+    private static void logException(PatOMat2Exception ex, HttpServletRequest request) {
+        LOG.error("Exception caught when processing request to '{}'.", request.getRequestURI(), ex);
+    }
+
+    @ExceptionHandler(PatOMat2Exception.class)
+    public ResponseEntity<ErrorInfo> handleException(PatOMat2Exception ex, HttpServletRequest request) {
+        logException(ex, request);
+        return new ResponseEntity<>(errorInfo(request, ex), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    private static ErrorInfo errorInfo(HttpServletRequest request, Throwable e) {
+        return ErrorInfo.createWithMessage(e.getMessage(), request.getRequestURI());
+    }
+
+    @ExceptionHandler(InvalidFileException.class)
+    public ResponseEntity<ErrorInfo> handleException(InvalidFileException ex, HttpServletRequest request) {
+        logException(ex, request);
+        return new ResponseEntity<>(errorInfo(request, ex), HttpStatus.BAD_REQUEST);
+    }
+}
