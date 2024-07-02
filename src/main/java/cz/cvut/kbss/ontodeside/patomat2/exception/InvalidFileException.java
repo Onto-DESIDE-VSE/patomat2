6149820e1be244712cf81dc9549ec4fa0b8b0355
Martin Ledvinka <martin.ledvinka@fel.cvut.cz>
2024-07-02 09:05:15 +0200

    Implement error handling.

---
+++ src/main/java/cz/cvut/kbss/ontodeside/patomat2/exception/InvalidFileException.java
@@ -0,0 +1,11 @@
+package cz.cvut.kbss.ontodeside.patomat2.exception;
+
+/**
+ * Indicates that an uploaded file is invalid.
+ */
+public class InvalidFileException extends PatOMat2Exception {
+
+    public InvalidFileException(String message) {
+        super(message);
+    }
+}
