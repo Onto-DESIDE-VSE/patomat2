e7052e06769f97d3d06907b5b216ec91097da5f6
Martin Ledvinka <martin.ledvinka@fel.cvut.cz>
2024-07-08 17:20:37 +0200

    [GH-5] Simplify pattern matches processing code.

--- src/main/java/cz/cvut/kbss/ontodeside/patomat2/service/OntologyHolder.java
+++ src/main/java/cz/cvut/kbss/ontodeside/patomat2/service/OntologyHolder.java
@@ -1,15 +1,35 @@
 package cz.cvut.kbss.ontodeside.patomat2.service;
 
-import cz.cvut.kbss.ontodeside.patomat2.service.pattern.ResultRow;
+import cz.cvut.kbss.ontodeside.patomat2.dto.PatternMatch;
 
 import java.io.File;
 import java.util.List;
 
+/**
+ * Manages ontology and allows finding matches in it.
+ */
 public interface OntologyHolder {
 
+    /**
+     * Whether ontology from the specified file has been loaded.
+     *
+     * @param fileName Ontology file name
+     * @return {@code true} if ontology from the specified file has been loaded
+     */
     boolean isLoaded(String fileName);
 
+    /**
+     * Loads ontology from the specified file.
+     *
+     * @param ontologyFile File containing ontology
+     */
     void loadOntology(File ontologyFile);
 
-    List<ResultRow> findMatches(File patternFile);
+    /**
+     * Finds matches of the pattern declared in the specified file in the loaded ontology.
+     *
+     * @param patternFile File containing pattern definition
+     * @return List of found matches
+     */
+    List<PatternMatch> findMatches(File patternFile);
 }
