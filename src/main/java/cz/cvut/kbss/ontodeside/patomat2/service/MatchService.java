9d7273db64dc0ee411a6b64e36e8c82a96891caa
Martin Ledvinka <martin.ledvinka@fel.cvut.cz>
2024-07-08 16:14:32 +0200

    [GH-5] Store matches in session-scoped bean.

--- src/main/java/cz/cvut/kbss/ontodeside/patomat2/service/MatchService.java
+++ src/main/java/cz/cvut/kbss/ontodeside/patomat2/service/MatchService.java
@@ -5,13 +5,19 @@ import cz.cvut.kbss.ontodeside.patomat2.dto.PatternMatch;
 import cz.cvut.kbss.ontodeside.patomat2.exception.OntologyNotUploadedException;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Service;
+import org.springframework.web.context.annotation.SessionScope;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Finds and applies pattern matches in ontology.
  */
+@SessionScope
 @Service
 public class MatchService {
 
@@ -20,6 +26,8 @@ public class MatchService {
     private final HttpSession session;
 
     private final OntologyHolder ontologyHolder;
+
+    private Map<Integer, PatternMatch> matches;
 
     public MatchService(FileStorageService storageService, HttpSession session, OntologyHolder ontologyHolder) {
         this.storageService = storageService;
@@ -35,11 +43,16 @@ public class MatchService {
         if (!ontologyHolder.isLoaded(ontologyFileName)) {
             final File ontologyFile = storageService.getFile(ontologyFileName);
             ontologyHolder.loadOntology(ontologyFile);
+            this.matches = new LinkedHashMap<>();
+        } else if (!matches.isEmpty()) {
+            return new ArrayList<>(matches.values());
         }
         final List<String> patternFiles = (List<String>) session.getAttribute(Constants.PATTERN_FILES_ATTRIBUTE);
         return patternFiles.stream().map(storageService::getFile).map(f -> ontologyHolder.findMatches(f).stream()
                                                                                          .map(r -> new PatternMatch(f.getName(), r.getBindings()))
                                                                                          .toList())
-                           .flatMap(List::stream).toList();
+                           .flatMap(List::stream)
+                           .peek(pm -> matches.put(pm.hashCode(), pm))
+                           .toList();
     }
 }
