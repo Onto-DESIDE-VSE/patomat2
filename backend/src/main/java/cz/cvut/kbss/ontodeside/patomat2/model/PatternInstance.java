1da76d7c2058acd4e74df5d26f7392a173f783cc
Martin Ledvinka <martin.ledvinka@fel.cvut.cz>
2024-07-30 17:51:30 +0200

    [Enhancement #21] Apply user-edited new entity labels in transformation.

--- backend/src/main/java/cz/cvut/kbss/ontodeside/patomat2/model/PatternInstance.java
+++ backend/src/main/java/cz/cvut/kbss/ontodeside/patomat2/model/PatternInstance.java
@@ -1,13 +1,14 @@
 package cz.cvut.kbss.ontodeside.patomat2.model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public record PatternInstance(int id, String patternName, PatternMatch match, String sparqlInsert, String sparqlDelete,
                               List<NewEntity> newEntities) {
 
     public PatternInstance deepCopy() {
-        return new PatternInstance(id, patternName, match, sparqlInsert, sparqlDelete, newEntities.stream()
-                                                                                                  .map(ne -> new NewEntity(ne.variableName(), ne.identifier(), ne.label()))
-                                                                                                  .toList());
+        return new PatternInstance(id, patternName, match, sparqlInsert, sparqlDelete, new ArrayList<>(newEntities.stream()
+                                                                                                                .map(ne -> new NewEntity(ne.variableName(), ne.identifier(), ne.label()))
+                                                                                                                .toList()));
     }
 }
--- frontend/src/types/PatternInstance.ts
+++ frontend/src/types/PatternInstance.ts
@@ -1,19 +1,26 @@
 export type PatternInstance = {
-  id: number
-  patternName: string
-  match: PatternMatch
-  sparqlInsert: string
-  sparqlDelete: string
-}
+  id: number;
+  patternName: string;
+  match: PatternMatch;
+  sparqlInsert: string;
+  sparqlDelete: string;
+  newEntities: NewEntity[];
+};
 
 export type PatternMatch = {
-  id: number
-  patternFile: string
-  bindings: ResultBinding[]
-}
+  id: number;
+  patternFile: string;
+  bindings: ResultBinding[];
+};
 
 export type ResultBinding = {
-  name: string
-  value: string
-  datatype: string
-}
+  name: string;
+  value: string;
+  datatype: string;
+};
+
+export type NewEntity = {
+  identifier: string;
+  variableName: string;
+  label: string;
+};
