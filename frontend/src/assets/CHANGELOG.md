3599c7f8758b10240148089afd19a9351233027a
Martin Ledvinka <martin.ledvinka@fel.cvut.cz>
2024-08-02 14:49:23 +0200

    [Enhancement #29] Add changelog accessible from the application About page.

---
+++ frontend/src/assets/CHANGELOG.md
@@ -0,0 +1,9 @@
+# Changelog
+
+---
+
+<br/>
+
+### 2024-07-30
+
+- Added support for editing new entity label
---
+++ frontend/src/components/Changelog.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+// @ts-ignore
+import changelog from "@/assets/CHANGELOG.md";
+</script>
+
+<template>
+  <div id="changelog" class="mt-6">
+    <changelog />
+  </div>
+</template>
+
+<style>
+#changelog ul {
+  padding-inline-start: 1rem;
+}
+</style>
