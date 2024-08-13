cda9c7c318521b1e8c8bf36952da0c46c233bc2c
Martin Ledvinka <martin.ledvinka@fel.cvut.cz>
2024-08-13 17:51:41 +0200

    [Enhancement #25] Init NLP pipeline used by name transformation functions on startup.
    The initialization is slow so take care of it on startup rather than on user request.

--- backend/src/main/java/cz/vse/swoe/ontodeside/patomat2/PatOMat2Application.java
+++ backend/src/main/java/cz/vse/swoe/ontodeside/patomat2/PatOMat2Application.java
@@ -1,5 +1,7 @@
 package cz.vse.swoe.ontodeside.patomat2;
 
+import cz.vse.swoe.ontodeside.patomat2.util.NLPPipelineProvider;
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
@@ -8,8 +10,13 @@ import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 @ConfigurationPropertiesScan
 public class PatOMat2Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PatOMat2Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PatOMat2Application.class, args);
+    }
 
+    @PostConstruct
+    public void initNLPPipeline() {
+        // Init the NLP pipeline on startup as it takes some time
+        NLPPipelineProvider.initNLPPipeline();
+    }
 }
