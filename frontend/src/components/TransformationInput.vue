eeab348a6ab66104d8a85ba533d6b3f497640ed4
Martin Ledvinka <martin.ledvinka@fel.cvut.cz>
2024-09-05 14:52:19 +0200

    [GH-51] Split transformation input view into tabs for import of files and urls.

---
+++ frontend/src/components/TransformationInput.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import OntologyImportFiles from "@/components/OntologyImportFiles.vue";
+import OntologyImportUrls from "@/components/OntologyImportUrls.vue";
+
+const activeTab = ref<"files" | "urls">("files");
+</script>
+
+<template>
+  <v-tabs v-model="activeTab" grow>
+    <v-tab value="files">Import from files</v-tab>
+    <v-tab value="urls">Import from URLs</v-tab>
+  </v-tabs>
+  <v-tabs-window v-model="activeTab">
+    <v-tabs-window-item value="files">
+      <OntologyImportFiles></OntologyImportFiles>
+    </v-tabs-window-item>
+    <v-tabs-window-item value="urls">
+      <OntologyImportUrls></OntologyImportUrls>
+    </v-tabs-window-item>
+  </v-tabs-window>
+</template>
+
+<style scoped>
+.v-window {
+  padding-top: 1rem;
+}
+</style>
