dae84655d24f889f87146bbd67e99d935133b7e4
Martin Ledvinka <martin.ledvinka@fel.cvut.cz>
2024-09-06 14:01:52 +0200

    [GH-56] Documentation for PatternInfo.

--- backend/src/main/java/cz/vse/swoe/ontodeside/patomat2/model/PatternInfo.java
+++ backend/src/main/java/cz/vse/swoe/ontodeside/patomat2/model/PatternInfo.java
@@ -2,6 +2,14 @@ package cz.vse.swoe.ontodeside.patomat2.model;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 
-@Schema(description = "Information")
-public record PatternInfo(String name, String fileName) {
+/**
+ * Information about a loaded pattern.
+ *
+ * @param name     Name of the pattern
+ * @param fileName Name of the file containing the pattern
+ */
+@Schema(description = "Information about a loaded pattern.")
+public record PatternInfo(
+        @Schema(description = "Name of the pattern") String name,
+        @Schema(description = "Name of the file containing the pattern") String fileName) {
 }
