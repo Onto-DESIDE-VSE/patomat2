0fa642f3072a13a0348059d1b7e6ba22cd173477
Martin Ledvinka <martin.ledvinka@fel.cvut.cz>
2024-09-26 08:30:51 +0200

    [GH-58] Support combined URL and file transformation input.

--- frontend/src/components/importing/TransformationInput.vue
+++ frontend/src/components/importing/TransformationInput.vue
@@ -2,23 +2,25 @@
 import { ref } from "vue";
 import OntologyImportFiles from "@/components/importing/OntologyImportFiles.vue";
 import OntologyImportUrls from "@/components/importing/OntologyImportUrls.vue";
+import OntologyImport from "@/components/importing/OntologyImport.vue";
 
 const activeTab = ref<"files" | "urls">("files");
 </script>
 
 <template>
-  <v-tabs v-model="activeTab" grow>
-    <v-tab value="files">Load from files</v-tab>
-    <v-tab value="urls">Load from URLs</v-tab>
-  </v-tabs>
-  <v-tabs-window v-model="activeTab">
-    <v-tabs-window-item value="files">
-      <OntologyImportFiles></OntologyImportFiles>
-    </v-tabs-window-item>
-    <v-tabs-window-item value="urls">
-      <OntologyImportUrls></OntologyImportUrls>
-    </v-tabs-window-item>
-  </v-tabs-window>
+  <OntologyImport></OntologyImport>
+  <!--  <v-tabs v-model="activeTab" grow>-->
+  <!--    <v-tab value="files">Load from files</v-tab>-->
+  <!--    <v-tab value="urls">Load from URLs</v-tab>-->
+  <!--  </v-tabs>-->
+  <!--  <v-tabs-window v-model="activeTab">-->
+  <!--    <v-tabs-window-item value="files">-->
+  <!--      <OntologyImportFiles></OntologyImportFiles>-->
+  <!--    </v-tabs-window-item>-->
+  <!--    <v-tabs-window-item value="urls">-->
+  <!--      <OntologyImportUrls></OntologyImportUrls>-->
+  <!--    </v-tabs-window-item>-->
+  <!--  </v-tabs-window>-->
 </template>
 
 <style scoped>
