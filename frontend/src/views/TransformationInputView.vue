0528cf26e78834089c05bedf076d7ed0fb76c9d3
Martin Ledvinka <martin.ledvinka@fel.cvut.cz>
2024-09-26 09:40:28 +0200

    [GH-58] Remove obsolete UI components.

--- frontend/src/views/TransformationInputView.vue
+++ frontend/src/views/TransformationInputView.vue
@@ -1,12 +1,12 @@
 <script setup lang="ts">
 import LoadedTransformationInputInfo from "@/components/importing/LoadedTransformationInputInfo.vue";
-import TransformationInput from "@/components/importing/TransformationInput.vue";
 import TransformationExamples from "@/components/importing/TransformationExamples.vue";
+import OntologyImport from "@/components/importing/OntologyImport.vue";
 </script>
 
 <template>
   <h3 class="text-h3 mb-6">Load Ontology and Transformation Patterns</h3>
   <LoadedTransformationInputInfo></LoadedTransformationInputInfo>
-  <TransformationInput></TransformationInput>
+  <OntologyImport></OntologyImport>
   <TransformationExamples></TransformationExamples>
 </template>
