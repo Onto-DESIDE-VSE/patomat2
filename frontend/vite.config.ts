6998acd1ab66cd75736868cda1c198c995bf5c43
Stanislav Vojíř <stanislav.vojir@gmail.com>
2025-04-08 23:07:24 +0200

    favicons, webmanifest - issue #82

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueDevTools from "vite-plugin-vue-devtools";
 import Markdown from "unplugin-vue-markdown/vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,7 +14,40 @@ export default defineConfig({
       include: [/\.vue$/, /\.md$/] // <-- allows Vue to compile Markdown files
     }),
     vueDevTools(),
-    Markdown({})
+    Markdown({}),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifestFilename: "manifest.json",
+      manifest: {
+        name: "PatOMat2",
+        short_name: "PatOMat2",
+        display: "browser",
+        background_color: "#ffffff",
+        theme_color: "#000000",
+        icons: [
+          {
+            src: "assets/favicon/favicon-32x32.png",
+            sizes: "32x32",
+            type: "image/png"
+          },
+          {
+            src: "assets/favicon/favicon-96x96.png",
+            sizes: "96x96",
+            type: "image/png"
+          },
+          {
+            src: "assets/favicon/favicon-144x144.png",
+            sizes: "144x144",
+            type: "image/png"
+          },
+          {
+            src: "assets/favicon/favicon-192x192.png",
+            sizes: "192x192",
+            type: "image/png"
+          }
+        ]
+      }
+    })
   ],
   resolve: {
     alias: {
