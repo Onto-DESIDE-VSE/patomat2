9a2ed36f2e595f2645278933754363ac3f3a313f
Stanislav Vojíř <stanislav.vojir@gmail.com>
2025-10-01 00:51:36 +0200

    export entity label in match bindings
    issue #69

--- backend/src/main/java/cz/vse/swoe/ontodeside/patomat2/model/ResultBinding.java
+++ backend/src/main/java/cz/vse/swoe/ontodeside/patomat2/model/ResultBinding.java
@@ -1,8 +1,16 @@
 package cz.vse.swoe.ontodeside.patomat2.model;
 
-public record ResultBinding(String name, String value, String datatype, boolean basedOnBlankNode) {
+import com.fasterxml.jackson.annotation.JsonInclude;
+import java.util.Optional;
+
+@JsonInclude(JsonInclude.Include.NON_ABSENT)
+public record ResultBinding(String name, String value, String datatype, boolean basedOnBlankNode, Optional<String> label) {
 
     public ResultBinding(String name, String value, String datatype) {
         this(name, value, datatype, false);
+    }
+
+    public ResultBinding(String name, String value, String datatype, boolean basedOnBlankNode) {
+        this(name, value, datatype, basedOnBlankNode, Optional.empty());
     }
 }
