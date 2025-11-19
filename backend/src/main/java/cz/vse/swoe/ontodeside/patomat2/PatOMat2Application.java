b6109a1e75f8adddd017f89df348ed06c6071f14
Martin Ledvinka <martin.ledvinka@fel.cvut.cz>
2025-11-19 12:09:31 +0100

    [GH-120] Load predefined patterns asynchronously.

--- backend/src/main/java/cz/vse/swoe/ontodeside/patomat2/PatOMat2Application.java
+++ backend/src/main/java/cz/vse/swoe/ontodeside/patomat2/PatOMat2Application.java
@@ -5,7 +5,9 @@ import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.scheduling.annotation.EnableAsync;
 
+@EnableAsync
 @SpringBootApplication
 @ConfigurationPropertiesScan
 public class PatOMat2Application {
